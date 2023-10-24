03eee330267847672da87bffae57b1245d9356b4
Blue Bird <bluebird089@proton.me>
2023-10-25 03:18:47 +0600

    added fucntion to delete cart item

--- index.js
+++ index.js
@@ -85,20 +85,19 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/cart/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id);
-            const query = { _id: new ObjectId(id) };
-            const result = await productsCollection.findOne(query);
-            res.send(result);
-        });
-
         app.post('/cart', async (req, res) => {
             const newProduct = req.body;
             const result = await cartCollection.insertOne(newProduct);
             res.send(result);
         });
-        
+
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
